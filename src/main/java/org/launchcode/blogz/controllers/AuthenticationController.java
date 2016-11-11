20e4369df8882aeb8a211d43e11e0696d15d5c6a
Brian Frick <Helmethair@zoho.com>
2016-11-10 20:31:18 -0600

    First push of assignment

--- src/main/java/org/launchcode/blogz/controllers/AuthenticationController.java
+++ src/main/java/org/launchcode/blogz/controllers/AuthenticationController.java
@@ -4,6 +4,8 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 import org.launchcode.blogz.models.User;
+import org.launchcode.blogz.models.dao.UserDao;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,6 +13,9 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class AuthenticationController extends AbstractController {
+	
+	@Autowired
+	private UserDao UserDao;
 	
 	@RequestMapping(value = "/signup", method = RequestMethod.GET)
 	public String signupForm() {
@@ -21,6 +26,30 @@ public class AuthenticationController extends AbstractController {
 	public String signup(HttpServletRequest request, Model model) {
 		
 		// TODO - implement signup
+		//username and password verify_error are part of template
+		String username = request.getParameter("name");
+		String password = request.getParameter("password");
+		String verify = request.getParameter("verify");
+		User Bob;
+		if(password.equals(verify)){
+			System.out.println("Password verify check");
+			if(User.isValidPassword(password)){
+				System.out.println("Password is valid.");
+				if(User.isValidUsername(username)){
+					System.out.println("Username is valid.");
+					Bob = new User(username, password);
+					UserDao.save(Bob);
+					System.out.println("Past Save Point");
+				}
+			}
+		}
+		//User sue = new User(username, password);
+		//UserDao.save(sue);
+		//validate username (no duplicates)
+		//validate password / create password hash / this  is done in user.java class
+		
+		//save all that stuff
+		
 		
 		return "redirect:blog/newpost";
 	}
